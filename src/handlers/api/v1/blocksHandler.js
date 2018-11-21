b8e266adaf4bb988bdd1496ecf7185e458b476f5
bencarle <ben@fullstacklabs.co>
2018-11-21 13:30:46 -0500

    Change block create payload to body

--- src/handlers/api/v1/blocksHandler.js
+++ src/handlers/api/v1/blocksHandler.js
@@ -22,11 +22,11 @@ async function show(req, res) {
 }
 
 async function create(req, res) {
-    if(!req.query.data) {
+    if(!req.body.data) {
         return res.status(500).send("Missing data query param!");
     }
 
-    const block = await Block.query().insert({data: req.query.data});
+    const block = await Block.query().insert({data: req.body.data});
     return res.status(201).send(Serializer.serialize('blocks', block.toJSON()));
 }
 
@@ -34,4 +34,4 @@ module.exports = {
     index,
     show,
     create
-}
+};
